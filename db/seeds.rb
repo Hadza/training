1afdcf611ae9b060ad8fdbba675b00fd6e9a1f81
Isaac Osuna <eddieisaac@gmail.com>
2022-06-05 18:59:28 -0600

    seeds for users and donations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = %w[Clothing Food Electronics Other]
+days_offset = rand(1..90)
+
+30.times do |i|
+  user = User.create(email: "user#{i.to_s}@example.com", password: "password")
+  user.update(created_at: Time.now - days_offset.days)
+  puts "Created user #{user.email} with created_at #{user.created_at}"
+end
+
+User.all.each do |user|
+  rand(5..15).times do |j|
+    donation = Donation.create(
+      name: "Donation #{j.to_s}",
+      description: "This is a donation #{j.to_s}",
+      category: categories.sample,
+      donor_id: user.id
+    )
+    donation.update(created_at: rand(user.created_at..Time.now))
+    puts "Created donation #{donation.name} with created_at #{donation.created_at}"
+  end
+end
