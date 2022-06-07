52da1a416e8d21658a07532943c9189ffd53a3e7
Isaac Osuna <eddieisaac@gmail.com>
2022-06-07 15:37:27 -0600

    create donation feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_194232) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_183426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "donations", force: :cascade do |t|
     t.text "name"
     t.text "description"
-    t.text "category"
     t.boolean "claimed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "donor_id", null: false
     t.bigint "donee_id"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_donations_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
