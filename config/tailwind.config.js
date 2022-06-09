690adc4125dfcad446bc37dbbb83743186c3c235
Isaac Osuna <eddieisaac@gmail.com>
2022-06-08 19:23:43 -0600

    base css for inputs and log in view

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,7 +17,16 @@ module.exports = {
         'primary-300': '#E8E6F6',
         'primary-500': '#8378DF',
         'primary-600': '#726DD9',
-        'primary-700': '#5F5FC9'
+        'primary-700': '#5F5FC9',
+        'light-gray': '#F9FAFC',
+        'gray-2': '#1F2D3D',
+        'gray-3': '#3C4858',
+        'gray-4': '#8492A6',
+        'gray-5': '#C2CEDB',
+        'gray-6': '#D3DCE6',
+        'gray-7': '#E5E9F2',
+        'gray-8': '#EFF2F7',
+        'gray-9': '#F9FAFC',
       }
     },
   },
